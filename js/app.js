91552812d87f84b5942f82b404e5aeb6c135828c
Jeff Mounts <mountzer@gmail.com>
2014-12-11 21:06:45 -0600

    bug

--- js/app.js
+++ js/app.js
@@ -25,15 +25,17 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
-  $('body').keydown(function(e){
-    if(e.keyCode == 88){
+  $('body').on('keydown', function(e) {
+    if(e.which == 88){
+      $('.ryu-still').hide();
       $('.ryu-ready').hide();
       $('.ryu-cool').show();
     }
   });
-  $('body').keyup(function(e){
-    if(e.keyCode == 88){
+  $('body').on('keyup', function(e){
+    if(e.which == 88){
       $('.ryu-cool').hide();
+      $('.ryu-still').hide();
       $('.ryu-ready').show();
     }
   });
